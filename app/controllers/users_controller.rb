559ad744a3c1c7dd43dc1b93f235b8b4c54af9f5
Gowrav N <gouravn2000@gmail.com>
2023-01-06 12:53:57 +0530

    Add restrictions in view level

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update]
   def show
-    @user = User.find(params[:id])
     @articles = @user.articles.paginate(page: params[:page], per_page: 4)
   end
 
   def index
-    @users = User.paginate(page: params[:page], per_page: 2)
+    @users = User.paginate(page: params[:page], per_page: 4)
   end
 
   def new
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       flash[
         :notice
       ] = "Welcome to Alpha Blog #{@user.username}, you have successfully signed up!"
@@ -26,11 +27,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account details was edited succesfully."
       redirect_to @user
@@ -43,5 +42,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
